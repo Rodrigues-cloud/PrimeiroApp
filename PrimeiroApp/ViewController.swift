748f44737d38f4fa776ccf843e4aeabed35340a5
Rodrigues-cloud <uesleyrs2020@outlook.com>
2020-04-02 21:56:16 -0300

    Adicionando o primeiro App

---
+++ PrimeiroApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Usuário on 21/12/19.
+//  Copyright © 2019 Uesley Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // exibir uma mensagem
+        print(" Meu Primeiro App")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
